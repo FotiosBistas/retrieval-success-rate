51a2bba759f3fe019e36a40b9b2d3547e9d8a5dd
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-09-22 05:00:48 +0300

    Random CID generation

---
+++ pkg/cids.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"github.com/ipfs/go-cid"
+	mh "github.com/multiformats/go-multihash"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+)
+
+func generate_random_cid() (cid.Cid, error) {
+
+	content := make([]byte, 1024)
+
+	if _, err := rand.Read(content); err != nil {
+		return cid.Cid{}, errors.Wrap(err, "while trying to generate random cid")
+	}
+
+	new_prefix := cid.Prefix{
+		Version:  1,
+		Codec:    cid.Raw,
+		MhType:   mh.SHA2_256,
+		MhLength: -1,
+	}
+
+	new_cid, err := new_prefix.Sum(content)
+	if err != nil {
+		return cid.Cid{}, errors.Wrap(err, " summing the content")
+	}
+	log.Info("New cid %s", new_cid.String())
+	return new_cid, nil
+}
