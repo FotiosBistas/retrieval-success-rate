294e08ece6ff9a1a9ace58505e51ed21ec3f5a19
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-09-27 21:51:08 +0300

    new config method

--- config/config.go
+++ config/config.go
@@ -1,14 +1,35 @@
 package config
 
-var local_ip = "127.0.0.1"
-var local_port = "8934"
+import (
+	"errors"
+	"github.com/urfave/cli/v2"
+)
 
-var default_config = Config{
-	log_level:      "info",
-	number_of_cids: 15,
+var Local_ip = "127.0.0.1"
+var Local_port = "8934"
+
+var Default_config = Config{
+	Log_level:      "info",
+	Number_of_cids: 15,
 }
 
 type Config struct {
-	log_level      string `json:"log-level"`
-	number_of_cids int    `json:"cid-number"`
+	Log_level      string `json:"log-level"`
+	Number_of_cids int    `json:"cid-number"`
+}
+
+func NewConfig(Cctx *cli.Context) (*Config, error) {
+	c := &Config{}
+	switch {
+	case Cctx.Command.Name == "optimistic_provide":
+		if Cctx.IsSet("log-level") {
+			c.Log_level = Cctx.String("log-level")
+		}
+		if Cctx.IsSet("cid-number") {
+			c.Number_of_cids = Cctx.Int("cid-number")
+		}
+	default:
+		return nil, errors.New("Unknown command found")
+	}
+	return c, nil
 }
