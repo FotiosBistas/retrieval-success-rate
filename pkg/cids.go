1e00a0667ab67243e03997e2cb7fd3efc09e0e98
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-10-04 11:18:02 +0300

    not enough data error maybe has to do with cid. Changed it to Dennis's optimistic provide generation of random cid

--- pkg/cids.go
+++ pkg/cids.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"crypto/sha256"
 	"github.com/ipfs/go-cid"
 	mh "github.com/multiformats/go-multihash"
 	"github.com/pkg/errors"
@@ -16,17 +17,16 @@ func GenerateRandomCid() (cid.Cid, error) {
 		return cid.Cid{}, errors.Wrap(err, "while trying to generate random cid")
 	}
 
-	new_prefix := cid.Prefix{
-		Version:  1,
-		Codec:    cid.Raw,
-		MhType:   mh.SHA2_256,
-		MhLength: -1,
+	hash := sha256.New()
+	hash.Write(content)
+
+	mhash, err := mh.Encode(hash.Sum(nil), mh.SHA2_256)
+
+	if err != nil {
+		return cid.Cid{}, errors.Wrap(err, "while generating multihash")
 	}
 
-	new_cid, err := new_prefix.Sum(content)
-	if err != nil {
-		return cid.Cid{}, errors.Wrap(err, " summing the content")
-	}
+	new_cid := cid.NewCidV0(mhash)
 	log.Infof("New cid %s", new_cid.String())
 	return new_cid, nil
 }
