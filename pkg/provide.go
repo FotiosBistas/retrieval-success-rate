e8cf89d3ffc96190a9bfb61e7b28c191bed70bce
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-10-04 08:33:01 +0300

    changed ctx given

--- pkg/provide.go
+++ pkg/provide.go
@@ -1,20 +1,19 @@
 package pkg
 
 import (
+	"context"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
-func StartProvidingEstimator(h *Host) error {
-	//ctx := context.Background()
+func StartProvidingEstimator(c context.Context, h *Host) error {
 
 	cid, err := GenerateRandomCid()
 	if err != nil {
 		return errors.Wrap(err, " trying to generate new cid")
 	}
 	log.Infof("Start providing content estimator for %s", cid.String())
-	//TODO provide Estimator is missing from dht
-	//err = h.dht.ProvideEstimator(ctx, cid)
+	err = h.DHT.OptimisticProvide(c, cid)
 	log.Infof("Done providing content estimator for cid %s", cid.String())
 	if err != nil {
 		return errors.Wrap(err, " when providing cid")
