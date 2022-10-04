f2c46d4cda4560586be388333676e07a7ecec73d
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-10-04 11:18:42 +0300

    info message doesn't print if operation not successful

--- pkg/provide.go
+++ pkg/provide.go
@@ -14,9 +14,9 @@ func StartProvidingEstimator(c context.Context, h *Host) error {
 	}
 	log.Infof("Start providing content estimator for %s", cid.String())
 	err = h.DHT.OptimisticProvide(c, cid)
-	log.Infof("Done providing content estimator for cid %s", cid.String())
 	if err != nil {
 		return errors.Wrap(err, " when providing cid")
 	}
+	log.Infof("Done providing content estimator for cid %s", cid.String())
 	return nil
 }
